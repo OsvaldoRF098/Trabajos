d56d370d1bfe0c38daee05eb8905fe8765eed11b
OsvaldoRF098 <osvaldoramirezflores098@gmail.com>
2024-09-23 19:35:37 -0600

    OMRF commit

---
+++ Exercise1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Por favor introduzca su nombre");
+int numeroPersonas= 12;
+string? NombrePersona= Console.ReadLine();
+Console.WriteLine("En este salon hay {0} personas y una se llama {1}",numeroPersonas,NombrePersona);
+Console.ReadKey();
